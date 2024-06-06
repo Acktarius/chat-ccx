cfe471641703200590daeaeb9284fa6295015329
acktarius <xchaps@aol.com>
2024-06-06 13:36:59 -0400

    installer

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ nodejs, npm, electron, kill-port
 ## Install
 within terminal, in folder of your choice :  
 `git clone https://github.com/Acktarius/chat-ccx.git` 
-## Option#1 
+
 ### Step 1
 Download the Model from hugging face :
 [https://huggingface.co/Acktarius/open_llama_3b_v2-w-loraCCX_2_Q8](https://huggingface.co/Acktarius/open_llama_3b_v2-w-loraCCX_2_Q8/tree/main)
@@ -24,19 +24,11 @@ in **chat-server** folder modify index.js to reflect the downloaded model.
 ie: `modelPath: path.join(__dirname, "models", "open_llama_3b_v2-w-loraCCX_2_Q8.gguf")`
 
 `npm install`  
-`node index.js`
-
 
 ### Step 3
 in **chat-react** folder
 `npm install`  
-`npm run dev`  
 
 
-### Step 4
-in your browser:  
-`http://localhost:4173/`  
-
-## Option#2
-run :  
-`./launcher/launcher.sh`  
+## Run
+`./launcher/launcher.sh` 
---
+++ install.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# shorcut installer for Chat-ccx for Ubuntu,Debian users
+# this file is subject to Licence
+# Copyright (c) 2024, Acktarius
+#
+# make sure ./install.sh is an executable file
+# otherwise, run: chmod 755 install.sh
+# run with command: ./install.sh
+#
+#
+#variables
+user=$(id -nu 1000)
+path=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+#Functions
+#trip
+trip() {
+kill -INT $$
+}
+## Shortcut creator
+shortcutCreator() {
+cat << EOF > /home/${user}/.local/share/applications/Conceal-chat.desktop
+[Desktop Entry]
+Encoding=UTF-8
+Name=Conceal chat-bot
+Path=${path}
+Exec=${path}/launcher/launch.sh
+Terminal=false
+Type=Application
+Icon=${path}/launcher/icon/chat-ccx.png
+Hidden=false
+NoDisplay=false
+Terminal=false
+Categories=Office
+X-GNOME-Autostart-enabled=true
+Comment=Chat about Conceal Network
+EOF
+echo "shortcut created, you may have to log out and log back in"
+}
+already() {
+read -p  "shortcut already in place, do you want to replace it (y/N)" ans
+	case $ans in
+		y | Y | yes)
+		rm -f /home/${user}/.local/share/applications/Conceal-chat.desktop
+		shortcutCreator
+		;;
+		*)
+		echo "nothing done"
+		;;
+	esac
+}
+# MAIN
+## check and install shortcut
+## not already install
+if [[ ! -f /home/${user}/.local/share/applications/Conceal-chat.desktop ]]; then 
+shortcutCreator
+else
+already
+fi
+## npm
+cd ${path}/chat-react
+npm install
+cd ${path}/chat-server
+npm install
+cd ..
+## other dep
+npm install -g electron -y
+npm install -g kill-port -y
